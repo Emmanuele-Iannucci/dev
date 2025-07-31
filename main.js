91bc4917e9d2a5a8225090d8accde43035d89043
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-31 19:19:33 +0200

    3

--- main.js
+++ main.js
@@ -10,9 +10,29 @@ for (let i = 0; i < nomi.length; i++) {
     console.log(nomi[i]);
 }
 */
-
+/*
 nomi.forEach((nome, index, array) => {
     console.log(nome);   
     console.log(index);
     console.log(array); 
 });
+*/
+const students = [
+  { firstName: 'Mario', lastName: 'Rossi' },
+  { firstName: 'Luigi', lastName: 'Verdi' },
+  { firstName: 'Margherita', lastName: 'Bianchi' }
+];
+// Raccogliamo in un array i nomi di tutti gli studenti
+// RISULTATO: ['Mario Rossi', 'Luigi Verdi', 'Marghertita Bianchi']
+
+let CompleteName = [];
+
+/* for (let i = 0; i < students.length; i++) {
+  let studente = students[i];
+  let nomeCompleto = studente.firstName + ' ' + studente.lastName;
+  CompleteName.push(nomeCompleto);
+}
+
+console.log(CompleteName);
+*/
+

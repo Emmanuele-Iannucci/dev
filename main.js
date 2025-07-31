060baedad84bf925a2798155d02610ca178d4db8
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-31 18:59:33 +0200

    2

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ let nomi = [
 
 ]
 console.log(nomi)
-
+/*
 for (let i = 0; i < nomi.length; i++) {
     console.log(nomi[i]);
 }
+*/
+
+nomi.forEach((nome, index, array) => {
+    console.log(nome);   
+    console.log(index);
+    console.log(array); 
+});
